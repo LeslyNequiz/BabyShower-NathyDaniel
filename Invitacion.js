08af675ed5ad400d822c553825f4382c85195ba2
LeslyNequiz <91855499+LeslyNequiz@users.noreply.github.com>
2022-01-04 23:10:15 -0600

    Update Invitacion.js

--- Invitacion.js
+++ Invitacion.js
@@ -2,7 +2,7 @@
             year: 2022, // required
             month: 1, // required
             day: 13, // required
-            hours: 10, // Default is 0 [0-23] integer
+            hours: 16, // Default is 0 [0-23] integer
             minutes: 0, // Default is 0 [0-59] integer
             seconds: 0, // Default is 0 [0-59] integer
             words: { //words displayed into the countdown
